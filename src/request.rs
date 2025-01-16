42f9dc088634a6c87aa84f7a304605507945ea9d
ShihHsuanChen <jack072315@gmail.com>
2025-01-16 16:04:36 +0800

    ADD: request body parsers

--- src/request.rs
+++ src/request.rs
@@ -18,7 +18,7 @@ pub struct Request<'a> {
     pub query: HashMap<String, String>,
     pub fragment: Option<String>,
     pub headers: HashMap<String,String>,
-    pub body: Vec<u8>,
+    pub body: ContentType,
 }
 
 
@@ -54,23 +54,52 @@ fn parse_readout_first_line(line: String) -> Result<(http::Protocol<'static>, ht
     Ok((protocol, method, url))
 }
 
-fn parse_readout_header_lines(line: String) -> Result<(String,String), String> {
-    match line.split_once(": ") {
-        Some(kv) => {
-            let (k, v) = kv;
-            Ok((String::from(k), String::from(v)))
-        },
-        None => Err(String::from("Fail to parse request header: {line}")),
+
+struct HeaderLine {
+    key: String,
+    value: String,
+    metadata: HashMap<String,String>,
+}
+
+
+fn parse_readout_header_line(line: &str) -> Result<HeaderLine, String> {
+    // Example line:
+    // Content-Disposition: form-data; name=\"b\"
+    // Content-Disposition: form-data; name=\"b\"; filename=\"abcabcabc.docx\"
+    let segs: Vec<_> = line.split("; ").collect();
+
+    if let Some(kv) = segs[0].split_once(": ") {
+        let (k, v) = kv;
+        let mut metadata = HashMap::<String,String>::new();
+        if segs.len() > 1 {
+            for seg in &segs[1..] {
+                if let Some(m) = seg.split_once("=") {
+                    let (mk, mut mv) = m;
+                    // unwrap \" from value
+                    if mv.starts_with("\"") { mv = &mv[1..] }
+                    if mv.ends_with("\"") { mv = &mv[..mv.len()-1] }
+                    metadata.insert(mk.to_string(), mv.to_string());
+                }
+            }
+        }
+        Ok(HeaderLine {
+            key: k.to_string(),
+            value: v.to_string(),
+            metadata,
+        })
+    } else {
+        return Err(String::from("Fail to parse request header: {line}"));
     }
 }
 
 /// Content-Types:
 /// - text/plain
 /// - text/html
+/// - text/css
 /// - application/xml
-/// - application/json
 /// - application/javascript
-pub fn parse_readout_body__raw_text(buf: &Vec<u8>) -> Result<String,String> {
+/// - ...
+pub fn parse_readout_body__text(buf: &Vec<u8>) -> Result<String,String> {
     match std::str::from_utf8(&buf) {
         Ok(s) => Ok(s.to_string()),
         Err(_) => Err(String::from("Fail to convert binary to string."))
@@ -79,16 +108,14 @@ pub fn parse_readout_body__raw_text(buf: &Vec<u8>) -> Result<String,String> {
 }
 
 /// Content-Types:
-/// - application/javascript
+/// - application/json
 pub fn parse_readout_body__json(buf: &Vec<u8>) -> Result<json::JsonValue,String> {
     // buf should be able to convert to utf-8 string
     match std::str::from_utf8(&buf) {
         Ok(json_str) => {
-            println!("'{json_str}'");
             match json::parse(json_str) {
                 Ok(v) => Ok(v),
                 Err(e) => {
-                    println!("{e:?}");
                     Err(String::from("Not a valid json string"))
                 }
             }
@@ -99,13 +126,167 @@ pub fn parse_readout_body__json(buf: &Vec<u8>) -> Result<json::JsonValue,String>
 
 /// Content-Types:
 /// - application/x-www-form-urlencoded
-pub fn parse_readout_body__x_www_form_urlencoded(buf: &Vec<u8>) -> Result<HashMap<String,String>,String> {
-    match std::str::from_utf8(&buf) {
-        Ok(s) => {
-            Ok(parse_urlencoded(s))
-        },
-        Err(_) => Err(String::from("Fail to convert binary to string."))
+pub fn parse_readout_body__x_www_form_urlencoded(buf: &Vec<u8>) -> Result<HashMap<String,ContentType>,String> {
+    if let Ok(s) = std::str::from_utf8(&buf) {
+        let mut res = HashMap::<String,ContentType>::new();
+        for (k,v) in parse_urlencoded(s) {
+            res.insert(k, ContentType::Text(v));
+        }
+        Ok(res)
+    } else {
+        Err(String::from("Fail to convert binary to string."))
     }
+}
+
+pub struct File {
+    filename: String,
+    filename_encoded: String,
+    content_type: Option<String>,
+    content: Vec<u8>,
+}
+
+pub struct Binary {
+    content_type: Option<String>,
+    content: Vec<u8>,
+}
+
+pub enum ContentType {
+    Text(String),
+    File(File),
+    Json(json::JsonValue),
+    Form(HashMap<String,ContentType>),
+    Binary(Binary),
+    None,
+}
+
+
+// TODO: better way?
+/// Content-Types:
+/// - multipart/form-data
+pub fn parse_readout_body__multipart(buf: &Vec<u8>, boundary: &str) -> Result<HashMap<String,ContentType>,String> {
+    // boundary=--------------------------896280056578890900126354
+    /* Example
+    ----------------------------896280056578890900126354
+    Content-Disposition: form-data; name="a"
+
+    1
+    ----------------------------896280056578890900126354
+    Content-Disposition: form-data; name="b"
+
+    asdasd
+    ----------------------------896280056578890900126354--\r\n
+    */
+
+    let extended_boundary = format!("--{boundary}");
+    let sep: &[u8] = &extended_boundary.as_bytes();
+    let l = sep.len();
+    let mut blocks: Vec<(Vec<String>,Vec<u8>)> = vec![];
+    let mut register: Vec<u8> = vec![];
+    let mut writting_header = false;
+    let mut writting_content = false;
+    let mut last: Option<u8> = None;
+    let mut header_lines: Vec<String> = vec![];
+
+    for v in buf.iter() {
+        if let Some(_v) = last {
+            // not linesep, append to register
+            if !(_v == 13 && *v == 10) {
+                register.push(_v);
+                last = Some(*v);
+                continue;
+            }
+            // meet linesep
+            let n = register.len();
+            if writting_content && n > l+2
+                && (&register[n-l..n] == sep || &register[n-l-2..n-2] == sep) {
+                // register: ...\r\n-------xxxxxxxxx
+                let content = &register[..n-l-2].to_vec();
+                blocks.push((header_lines.clone(), content.clone()));
+                header_lines.clear();
+                register.clear();
+                last = None;
+                writting_header = true;
+                writting_content = false;
+            } else if &register == sep { // meet boundary
+                // register: ...-------xxxxxxxxx
+                writting_header = true;
+                register.clear();
+                last = None;
+            } else if writting_header {
+                if let Ok(line) = std::str::from_utf8(&register) {
+                    let _line = line.to_string();
+                    if _line == "" {
+                        // blank line as the segarator of header and content
+                        writting_header = false;
+                        writting_content = true;
+                    } else {
+                        header_lines.push(line.to_string());
+                    }
+                    last = None;
+                } else {
+                    return Err(String::from("Fail to parse form-data/multipart block header."));
+                }
+                register.clear();
+            } else {
+                // writting content but not meet boundary yet
+                register.push(_v);
+                last = Some(*v);
+            }
+        } else {
+            last = Some(*v);
+        }
+    }
+    let mut res = HashMap::<String,ContentType>::new();
+    for (block,content) in blocks.iter() {
+        let mut headers = HashMap::<String, HeaderLine>::new();
+        for line in block.iter() {
+            if let Ok(header) = parse_readout_header_line(line) {
+                headers.insert(header.key.to_string(), header);
+            }
+        }
+        if let Some(header) = headers.get("content-Disposition") {
+            if let Some(key) = header.metadata.get("name") {
+                if let Some(filename) = header.metadata.get("filename") {
+                    // File
+                    let v = ContentType::File(File {
+                        filename: filename.to_string(),
+                        filename_encoded: {
+                            header.metadata.get("filename*")
+                            .unwrap_or(filename)
+                            .to_string()
+                        },
+                        content_type: {
+                            match header.metadata.get("Content-Type") {
+                                Some(v) => Some(v.to_string()),
+                                None => None,
+                            }
+                        },
+                        content: content.to_vec(),
+                    });
+                    res.insert(key.to_string(), v);
+                } else if let Ok(s) = std::str::from_utf8(content) {
+                    let v = ContentType::Text(s.to_string());
+                    res.insert(key.to_string(), v);
+                } else {
+                    println!("Fail to parse mutipart/form-data content of '{key}'");
+                }
+            } else {
+                // No name
+            }
+        } else {
+            // No Content-Disposition
+        }
+    }
+    Ok(res)
+}
+
+/// Content-Types:
+/// - application/x-www-form-urlencoded
+pub fn parse_readout_body__binary(buf: &Vec<u8>, content_type: &str) -> Result<ContentType,String> {
+    Ok(ContentType::Binary( Binary {
+        content_type: Some(content_type.to_string()),
+        content: buf.to_vec(),
+    }))
 }
 
 struct ParseResult {
@@ -114,6 +295,7 @@ struct ParseResult {
     url: Option<Url>,
     headers: Option<HashMap::<String,String>>,
     body: Option<Vec<u8>>,
+    boundary: Option<String>,
 }
 
 
@@ -133,7 +315,6 @@ pub fn parse_urlencoded(s: &str) -> HashMap<String,String> {
 fn parse_readout(buf_reader: &mut BufReader<&TcpStream>) -> Result<ParseResult, String> {
     let mut register: Vec<u8> = vec![];
     let mut last: Option<u8> = None;
-    let mut iter = buf_reader.bytes();
     let mut end_of_header = false;
     let mut cl: u32 = 0;
     let mut iline: u32 = 0;
@@ -145,19 +326,18 @@ fn parse_readout(buf_reader: &mut BufReader<&TcpStream>) -> Result<ParseResult, 
         url: None,
         headers: None,
         body: None,
+        boundary: None,
     };
-    while let Some(byte) = iter.next() {
-        // println!("{end_of_header}");
-        if !byte.is_ok() { break; }
+    for byte in buf_reader.bytes() {
         let v = byte.unwrap();
         if let Some(_v) = last {
-            // not lineseq, append to register
+            // not linesep, append to register
             if !(_v == 13 && v == 10) {
                 register.push(_v);
                 last = Some(v);
                 continue;
             }
-            // meet lineseq line
+            // meet linesep line
             if let Ok(line) = std::str::from_utf8(&register) {
                 let _line = line.to_string();
                 if _line == "" {
@@ -175,13 +355,21 @@ fn parse_readout(buf_reader: &mut BufReader<&TcpStream>) -> Result<ParseResult, 
                         Err(e) => return Err(e),
                     }
                 } else {
-                    match parse_readout_header_lines(_line) {
-                        Ok(kv) => {
-                            let (k, v) = kv;
-                            if k == "Content-Length" {
-                                cl = v.parse().unwrap();
+                    match parse_readout_header_line(&_line) {
+                        Ok(h) => {
+                            let (hk, hv) = (h.key, h.value);
+                            if hk == "Content-Length" {
+                                cl = hv.parse().unwrap();
+                                headers.insert(hk, hv);
+                            } else if hk == "Content-Type" {
+                                if let Some(b) = h.metadata.get("boundary") {
+                                    result.boundary = Some(b.to_string());
+                                } else {
+                                    headers.insert(hk, hv);
+                                }
+                            } else {
+                                headers.insert(hk, hv);
                             }
-                            headers.insert(k, v);
                         },
                         Err(e) => return Err(e),
                     }
@@ -231,7 +419,46 @@ impl Request<'_> {
         };
         let headers = res.headers.unwrap();
         let body = res.body.unwrap();
-        println!("{headers:?}");
+        let body_boundary = res.boundary;
+        // content-type
+        let none = String::from("none");
+        let content_type = &headers.get("Content-Type").unwrap_or(&none)[..];
+        let content = match content_type {
+            "multipart/form-data" => {
+                if body_boundary.is_none() {
+                    return Err(String::from("Multipart/form-data boundary not found"));
+                }
+                let boundary = body_boundary.unwrap();
+                match parse_readout_body__multipart(&body, &boundary) {
+                    Ok(res) => ContentType::Form(res),
+                    Err(e) => return Err(e),
+                }
+            },
+            "application/json" => {
+                match parse_readout_body__json(&body) {
+                    Ok(res) => ContentType::Json(res),
+                    Err(e) => return Err(e),
+                }
+            },
+            "application/x-www-form-urlencoded" => {
+                match parse_readout_body__x_www_form_urlencoded(&body) {
+                    Ok(res) => ContentType::Form(res),
+                    Err(e) => return Err(e),
+                }
+            },
+            "application/javascript" | "text/javascript" |
+            "application/css" | "text/css" |
+            "text/html" | "text/plain" |
+            "application/xml" | "text/xml" => {
+                match parse_readout_body__text(&body) {
+                    Ok(res) => ContentType::Text(res),
+                    Err(e) => return Err(e),
+                }
+            },
+            _ => {
+                ContentType::None
+            }
+        };
         Ok(Request {
             protocol,
             method,
@@ -241,7 +468,7 @@ impl Request<'_> {
             query,
             fragment,
             headers,
-            body,
+            body: content,
         })
     }
 }
